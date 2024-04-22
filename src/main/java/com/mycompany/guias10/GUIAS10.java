deb68bed01799a3172f5128a03d31422997dc712
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 19:00:34 -0500

    primer GUIA10

---
+++ src/main/java/com/mycompany/guias10/GUIAS10.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guias10;
+
+import igui.pantallas10;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIAS10 {
+
+    public static void main(String[] args) {
+         new pantallas10().setVisible(true);
+    }
+}
